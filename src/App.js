66477a5484a07eab69f68c10c89dff7398c44aea
JimCoder-Dev <jamesholmson88@gmail.com>
2022-06-30 09:41:14 +0100

    Gradient in the header

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ const App = () => {
   return (
     <StrictMode>
       <ThemeContext.Provider value={theme}>
+        <div className="p-0 m-0" style={{
+    background: "url(http://pets-images.dev-apis.com/pets/wallpaperC.jpg)",
+  }}> 
         <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me</Link>
+          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+            <Link className="text-6xl text-white hover:text-gray-200" to="/">Adopt Me</Link>
           </header>
 
           <Routes>
@@ -21,6 +24,8 @@ const App = () => {
             <Route path="/" element={<SearchParams />} />
           </Routes>
         </BrowserRouter>
+        </div>
+        
       </ThemeContext.Provider>
     </StrictMode>
   );
